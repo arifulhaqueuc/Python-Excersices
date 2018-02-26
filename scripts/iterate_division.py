82138cb8971d89625e483dee159f97235fb82852
Ariful Haque <arifulhaque2010@yahoo.com>
2018-02-26 17:33:30 -0500

    Create iterate_division.py

---
+++ scripts/iterate_division.py
@@ -0,0 +1,31 @@
+
+
+print "Input Your 1st number"
+num1 = input()
+
+print "Input Your 2nd number"
+num2 = input()
+
+
+print "Input Your 3rd number"
+num3 = input()
+
+
+print "Your number is ", num1, num2, num3
+##print "Your number is ", num2
+##print "Your number is ", num3
+x = int(num1+num2+num3)
+
+m = (float(num1)/x)*100
+n = (float(num2)/x)*100
+p = (float(num3)/x)*100
+##print "Sum of them is: ", x
+
+print "for ", num1
+print "The results are: ", m
+print "for ", num2
+print "The results are: ", n
+print "for ", num3
+print "The results are: ", p
+
+
