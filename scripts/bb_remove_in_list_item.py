010c138eba95b29e10016838444dbe90cfc06b58
Ariful Haque <arifulhaque2010@yahoo.com>
2018-02-28 17:21:13 -0500

    Update bb_remove_in_list_item.py

--- scripts/bb_remove_in_list_item.py
+++ scripts/bb_remove_in_list_item.py
@@ -2,7 +2,7 @@
 ## Print the items form the following list
 ## if the items ONLY start with "ba"
 
-list_all = [
+list_all_1 = [
 
 
  'bb1'
@@ -14,8 +14,32 @@ list_all = [
 ]
 
 
-bb_remove = [x for x in list_all if x[:2]=='ba']
+bb_remove = [x for x in list_all_1 if x[:2]=='ba']
 
 for i in ba_remove:
 	print i
+
+
+
+	
+list_all_2 = [
+
+
+ 'b000b1'
+,'b000b2'
+,'b000b3'
+,'b000a4'
+
+
+]
+	
+## let's print the items if 
+## "ba" is in a item
+
+ba_remove = [s for s in list_all if 'ba' in s]
+for i in ba_remove:
+
+	print i
+
+
 
