2deed8930ce9c83978a9804df894398490619c89
Ariful Haque <arifulhaque2010@yahoo.com>
2018-05-08 20:13:33 -0400

    Add file permission script

---
+++ file_permission.py
@@ -0,0 +1,15 @@
+from os.path import isfile
+
+filename = "react dev sources.txt"
+
+print("Asking for permission")
+
+
+if isfile(filename):
+	f = open(filename)
+	print("File named %s found" % filename)
+	f.close()
+else: 
+	print("Nof file named %s found" % filename)
+
+print("Job done")	
