8b2b59a1d6cb19947f51f571541ce6721dbf6f7c
Ariful Haque <arifulhaque2010@yahoo.com>
2018-05-18 03:14:44 -0400

    Update Lambda expressions.py

--- scripts/Lambda expressions.py
+++ scripts/Lambda expressions.py
@@ -1,38 +1,15 @@
 ## Lambda expressions
+## Definition: They are anonnymus functions, meaning that they donot have any function name.
 ## We cannot use lambda experessions for multi-line functions
-## common application: sorting and filtering data
+## real-life example: sorting and filtering data i.e. sorting people based on their last name
+
+## simple example 
+## lambda function with one input
+x = lambda x:3*x +1
+print(x(2))
 
 
-## They are anonnymus functions, meaning that they donot have any function name.
-
-
-## General approach
-
-def func(x):
-	return 3*x + 1
-
-
-func(2) ## >> 7
-
-## Labmda approach
-## formula
-## lambda input: expression
-## with only one input
-
-## example 1
-
-lambda x: 3*x + 1
-
-
-## example 2
-
-g = 4lambda x: 3*x + 1
-
-g(2)
-
-
-## with more than two inputs
-## example: combine first and last name
-
-full_name = lambda fn, ln: fn.strip().title() + " " + ln.strip().title()
-full_name(" ariful", "HAQUE" )
+## real-life example 
+## lambda function with more than one input
+name = lambda first, last: first.strip() + " " + last.strip()
+print(name("ariful", "haque"))
