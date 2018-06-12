c410923d3d1dbbf23946c33ecde677d89f6cd591
Ariful Haque <arifulhaque2010@yahoo.com>
2018-06-12 16:05:31 -0400

    Create car1.py

---
+++ oop/car1.py
@@ -0,0 +1,21 @@
+
+
+
+class Car:
+	make = "Nissan"
+	model = "Sentra"
+	year = "2010"
+
+
+
+x = Car()
+y = Car()
+
+print("This is the value of instance 1")
+print(x.make)
+
+print("This is the value of instance 2")
+print(y.make)
+
+
+
